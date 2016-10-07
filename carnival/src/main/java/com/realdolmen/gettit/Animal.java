86f40821c4e09fc6a73e8606305647a77952f819
Melvin Hulsmans <Melvin.Hulsmans@gmail.com>
2016-10-07 15:24:30 +0200

    let the party begin

---
+++ carnival/src/main/java/com/realdolmen/gettit/Animal.java
@@ -0,0 +1,19 @@
+package com.realdolmen.gettit;
+
+public class Animal {
+	String name;
+	Integer legs;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getLegs() {
+		return legs;
+	}
+	public void setLegs(Integer legs) {
+		this.legs = legs;
+	}	
+}
---
+++ carnival/src/main/java/com/realdolmen/gettit/App.java
@@ -0,0 +1,33 @@
+package com.realdolmen.gettit;
+
+import java.util.ArrayList;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+    public static void main( String[] args )
+    {
+    	ArrayList<Animal> a = new ArrayList<Animal>();
+    	Animal b = new Animal();
+    	b.legs = 4;
+    	b.name = "Lion";
+    	a.add(b);
+    	b.legs = 8;
+    	b.name = "Spider";
+    	a.add(b);
+        System.out.println( "Welcome to out circus with the magnificent animals, we got:" );
+        
+        for(int i = 0; i < a.size(); --i){
+        	System.out.println( a.get(i).name );
+        }
+        
+    }
+    
+    
+    
+    
+}
